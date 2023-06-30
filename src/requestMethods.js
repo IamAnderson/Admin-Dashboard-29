9fc8df1992355bdc9182d430f8e28b6767b86fe3
Anderson <omorogbeandersonsimeon@gmail.com>
2023-06-29 22:37:59 -0700

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://two9server.onrender.com/api/";
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.accessToken
 
 // const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNGQxYWRiZTgwNzJiMTljZGFmMmRlYSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NzE4ODQ4MDQsImV4cCI6MTY3MjE0NDAwNH0.W-M-OEAHKhLZsKdl4w_jvMWwiGkP_qHBIIXI6x7Uy-g"
 
